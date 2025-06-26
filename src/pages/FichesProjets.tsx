e6a8aa3b51b33d610408cc09eebea86e2415bb4f
CristianPZA <cristian.pinzari@quant.global>
2025-06-26 14:30:32 +0200

    Updated FichesProjets.tsx

--- src/pages/FichesProjets.tsx
+++ src/pages/FichesProjets.tsx
@@ -800,11 +800,6 @@ const FichesProjets = () => {
                 ? 'border-indigo-500 text-indigo-600'
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
             }`}
-          >
-            <div className="flex items-center gap-2">
-              <User className="w-5 h-5" />
-              Mes projets collaborateur ({projetsCollaborateur.length})
-            </div>
           </button>
         </nav>
       </div>
